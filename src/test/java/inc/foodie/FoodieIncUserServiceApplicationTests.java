25529b2884fdae9c7a9713eb59417e507d1e2084
Burton Arceneaux <btarceneaux@gmail.com>
2023-11-15 13:00:44 -0800

    Initial commit.

---
+++ src/test/java/inc/foodie/FoodieIncUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.foodie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieIncUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
