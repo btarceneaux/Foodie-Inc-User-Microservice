9d2cf0172af59df807aea90633a54971c0e80f19
Burton Arceneaux <btarceneaux@gmail.com>
2023-12-25 16:46:07 -0800

    Added CORS configuration.

--- src/main/java/inc/foodie/FoodieIncUserServiceApplication.java
+++ src/main/java/inc/foodie/FoodieIncUserServiceApplication.java
@@ -4,11 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableDiscoveryClient
 public class FoodieIncUserServiceApplication {
 
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.addAllowedOrigin("*");
+		corsConfig.addAllowedMethod("*");
+		corsConfig.addAllowedHeader("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FoodieIncUserServiceApplication.class, args);
 	}
